22484bb3692e816963990dd02ba9d3dd524e714f
Marharyta Derevianko <margoshka.ri6@gmail.com>
2023-07-27 17:20:56 +0300

    add header

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 export default function Main(){
+    const [isAuth, setIsAuth] = useState(false);
     return (
         <div className="app">
-
+            <div className="content">
+                <header className="header">
+                    <div className="header-left">
+                        <a className="header-text-logo">Closely</a>
+                    </div>
+                    <div className="header-right">
+                        {isAuth ? <></> : 
+                            <a className="header-text">Увійти</a>
+                        }
+                    </div>
+                </header>
+                <div className="line"></div>
+            </div>
         </div>
     );
 }
